882ec0682b8c715b2e6fcad22ca2faef778de2a4
yol256 <yonglee16@gmail.com>
2023-03-26 10:24:03 -0400

    assignment 4

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var {engine} = require('express-handlebars')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var taskRouter = require('./routes/task');
 
 var app = express();
 
 // view engine setup
+app.engine('handlebars', engine());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', taskRouter); // all the routes are in the taskRouter
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,10 +32,10 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log(err.message)
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {title: 'Error', error: err.message});
 });
 
 module.exports = app;
